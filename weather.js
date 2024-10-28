94317bb6962b4f9dc8c4e94f09128a387eb0b7c6
Gokul <gokulkrishnagokulam2001@gmail.com>
2024-10-28 22:48:38 +0530

    ver 0.2

--- weather.js
+++
@@ -1,9 +0,0 @@
-let result = fetch(
-  "https://api.openweathermap.org/data/2.5/weather?q=kochi&appid=61c12bce12b7729f748f3fff49a18816"
-);
-
-result
-  .then((res) => res.json())
-  .then((e) => {
-    console.log(e);
-  });
